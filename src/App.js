9249e24dad427c8b1acda991ab1388b0108113dd
Aneta Ryglova <aneta.ryglova@asseco-ce.com>
2022-04-27 08:36:38 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const URL = 'http://data.fixer.io/api/latest?access_key=b033e4b0e14a15684e3580af0fc09a56'
 
 function App() {
+
+  const [currency, setCurrency] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const [exchange, setExchange] = useState()
+  
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchange
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchange
+  }
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        setCurrency([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        //setToCurrency(data.rates[Object.keys(data.rates)[0]])
+        setToCurrency('CZK')
+        setExchange(data.rates[Object.keys(data.rates)[0]])
+      })
+  }, [])
+
+  useEffect(() => {
+    if (fromCurrency !== null && toCurrency !== null && fromCurrency !== 'EUR') {
+      fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+          setExchange(data.rates[toCurrency]/data.rates[fromCurrency])
+        })
+    }
+    else if (fromCurrency !== null && toCurrency !== null && fromCurrency === 'EUR') {
+      fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+          setExchange(data.rates[toCurrency])
+        })
+    }
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow currency={currency} selectedCurrency={fromCurrency} onChangeCurrency={e => setFromCurrency(e.target.value)} amount={fromAmount} onChangeAmount = {handleFromAmountChange}/>
+      <div>=</div>
+      <CurrencyRow currency={currency} selectedCurrency={toCurrency} onChangeCurrency={e => setToCurrency(e.target.value)} amount={toAmount} onChangeAmount = {handleToAmountChange}/>
+    </>
   );
+  
 }
 
 export default App;
